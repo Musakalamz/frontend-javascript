c7cec3ef07e0b70169029127cc101bae6eb857de
Musakalamz <ogunsolumusa@gmail.com>
2025-09-09 23:24:23 +0100

    1-Creating an interface for a student

--- task_0/js/main.ts
+++ task_0/js/main.ts
@@ -21,69 +21,24 @@ const studentB: Student = {
 
 const studentsList: Array<Student> = [studentA, studentB];
 
-const styleSheet = `
-  html {
-    margin: 0;
-    height: 100%;
-  }
-  body {
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 80%;
-    margin: 10%;
-  }
-  table {
-    border-collapse: collapse;
-  }
-  thead {
-    font-weight: bold;
-  }
-  td {
-    padding: 10px;
-    border: 1px solid gray;
-    cursor: pointer;
-  }
-  td:hover {
-    background: gainsboro;
-  }
+const table = document.createElement("table");
+table.border = "1";
 
-  td:nth-child(1) {
-    text-align: center;
-  }
-`;
+const headerRow = table.insertRow();
 
-/**
- * Displays information about students in a table.
- * @param students The list of students to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
+const headerCell1 = headerRow.insertCell();
+headerCell1.textContent = "First Name";
 
-export const displayStudents = (students: Array<Student>): void => {
-  const table = document.createElement("table");
-  const tableHead = document.createElement("thead");
-  const headRow = document.createElement("tr");
-  const tableBody = document.createElement("tbody");
+const headerCell2 = headerRow.insertCell();
+headerCell2.textContent = "Location";
 
-  headRow.insertAdjacentHTML("beforeend", "<td>FirstName</td");
-  headRow.insertAdjacentHTML("beforeend", "<td>Location</td");
-  tableHead.insertAdjacentElement("beforeend", headRow);
+studentsList.forEach((student) => {
+  const row = table.insertRow();
+  const firstNameCell = row.insertCell();
+  firstNameCell.textContent = student.firstName;
 
-  for (const student of students) {
-    const bodyRow = document.createElement("tr");
-    bodyRow.insertAdjacentHTML("beforeend", `<td>${student.firstName}</td>`);
-    bodyRow.insertAdjacentHTML("beforeend", `<td>${student.location}</td>`);
-    tableBody.insertAdjacentElement("beforeend", bodyRow);
-  }
+  const locationCell = row.insertCell();
+  locationCell.textContent = student.location;
+});
 
-  table.insertAdjacentElement("beforeend", tableHead);
-  table.insertAdjacentElement("beforeend", tableBody);
-  document.body.insertAdjacentElement("beforeend", table);
-};
-
-displayStudents(studentsList);
-const styleSheetElement = document.createElement("style");
-styleSheetElement.innerHTML = styleSheet;
-document.head.insertAdjacentElement("beforeend", styleSheetElement);
-document.title = "Task 0";
+document.body.appendChild(table);
