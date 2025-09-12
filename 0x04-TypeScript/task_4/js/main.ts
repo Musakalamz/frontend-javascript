bba3b14d209f294c508e83a9de9271d909fb7a68
Musakalamz <ogunsolumusa@gmail.com>
2025-09-12 12:51:40 +0100

    Update task_4/js/main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,23 +1,24 @@
-export const cpp: Subjects.Cpp = new Subjects.Cpp();
-export const java: Subjects.Java = new Subjects.Java();
-export const react: Subjects.React = new Subjects.React();
-export const cTeacher: Subjects.Teacher = {
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
+
+export const cTeacher: Teacher = {
   firstName: "Dennis",
   lastName: "Ritchie",
   experienceTeachingC: 10,
 };
 
 console.log("C++");
-cpp.setTeacher = cTeacher;
+cpp.setTeacher(cTeacher);
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
 console.log("Java");
-java.setTeacher = cTeacher;
+java.setTeacher(cTeacher);
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
 console.log("React");
-react.setTeacher = cTeacher;
+react.setTeacher(cTeacher);
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
