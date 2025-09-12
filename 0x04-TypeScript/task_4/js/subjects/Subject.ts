c96903969c8fe28788a299362433363fb91e6bed
Musakalamz <ogunsolumusa@gmail.com>
2025-09-12 12:31:22 +0100

    Namespace & Declaration merging

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -2,7 +2,7 @@ namespace Subjects {
   export class Subject {
     teacher: Subjects.Teacher;
 
-    public set setTeacher(teacher: Subjects.Teacher) {
+    public set setTeacher(teacher: Teacher) {
       this.teacher = teacher;
     }
   }
