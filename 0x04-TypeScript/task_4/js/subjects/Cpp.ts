69e36a41fed535783e72b296c6c1e099c61d598d
Musakalamz <ogunsolumusa@gmail.com>
2025-09-14 10:27:20 +0100

    Bug Err0r - Condition statement fixed

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -8,7 +8,7 @@ namespace Subjects {
       return "Here is the list of requirements for Cpp";
     }
     getAvailableTeacher(): string {
-      if (!this.teacher || this.teacher.experienceTeachingC < +0) {
+      if (!this.teacher || this.teacher.experienceTeachingC <= 0) {
         return " No available teacher";
       }
       return `Available Teacher: ${this.teacher.firstName}`;
